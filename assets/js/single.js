a9318b56c408f4499216cb206adc9cb052505e5e
Adam Work <adamcrandall91@gmail.com>
2020-11-03 16:28:17 -0500

    6.3.6

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
+
+
+
 var getRepoIssues = function(repo) {
     console.log(repo);
 
@@ -12,6 +29,11 @@ var getRepoIssues = function(repo) {
                 displayIssues(data);
             });
         }
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
+        }
+
         else {
             alert("There was a problem with your request!");
         }
