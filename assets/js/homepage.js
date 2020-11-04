8de922482fa1214d8c5f5f43c47aeaf0f33997c0
Adam Work <adamcrandall91@gmail.com>
2020-11-04 16:14:16 -0500

    6.5.7

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,7 @@
 // Dom Elements for repo display
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
 // Gets github repos by username
@@ -98,6 +99,35 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl ="https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues"
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });         
+        }
+        else {
+            alert("Error: " + response.statusText);
+        }
+    });
+}
+
+var buttonClickHandler = function(event) {
+     var language = event.target.getAttribute("data-language");
+     
+     if (language) {
+         getFeaturedRepos(language);
+
+         //clear old content
+         repoContainerEl.textContent = "";
+     }
+};
+
 
 // Event Listener for when "Get User" is clicked
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+// Event Listern for when language buttons are clicked
+languageButtonsEl.addEventListener("click", buttonClickHandler);
